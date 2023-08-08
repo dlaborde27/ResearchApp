4135337a152940f79d4455c7683e8c045c134df2
dlaborde27 <107769722+dlaborde27@users.noreply.github.com>
2023-08-07 21:17:21 -0500

    xd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ export function Footer() {
                 <div class="footer-content">
                     <h3>Laboratorio de Bioinformática y Aprendizaje Autónomo - LBA<sup>2</sup></h3>
                     <ul class="socials">
-                        <li><a href="https://www.youtube.com/@espol1" target="_blank"><i className="fab fa-instagram"></i></a></li>
-                        <li><a href="https://www.youtube.com/@espol1" target="_blank"><i className="fab fa-youtube"></i></a></li>
-                        <li><a href="https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Aorganization%3A15092634&keywords=escuela%20superior%20polit%C3%A9cnica%20del%20litoral&origin=RICH_QUERY_SUGGESTION&position=1&searchId=77076dd2-844a-4976-ac98-3b59483e518a&sid=LPK" target="_blank"><i className="fab fa-linkedin"></i></a></li>
+                        <li><a href="https://instagram.com/lba2ec?igshid=MzRlODBiNWFlZA==" target="_blank"><i className="fab fa-instagram"></i></a></li>
+                        {/* <li><a href="https://www.youtube.com/@espol1" target="_blank"><i className="fab fa-youtube"></i></a></li>
+                        <li><a href="https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Aorganization%3A15092634&keywords=escuela%20superior%20polit%C3%A9cnica%20del%20litoral&origin=RICH_QUERY_SUGGESTION&position=1&searchId=77076dd2-844a-4976-ac98-3b59483e518a&sid=LPK" target="_blank"><i className="fab fa-linkedin"></i></a></li> */}
                     </ul>
                 </div>
                 <div class="footer-bottom">
